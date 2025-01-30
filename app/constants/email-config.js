3d3998b1d081f7531cfd6d1e7cdf1e4bf3ee02a5
John Barnard <john.barnard.external@eviden.com>
2025-01-30 11:46:16 +0000

    Corrected typos

--- app/constants/email-config.js
+++ app/constants/email-config.js
@@ -3,21 +3,21 @@ const events = require('./events')
 const sourceSystems = require('./source-systems')
 
 module.exports = {
-  [events.PROCESSING_SUBCRIPTION_FAILED]: {
+  [events.PROCESSING_SUBSCRIPTION_FAILED]: {
     [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.BPS]: `${alertConfig.bpsEmails};${alertConfig.devTeamEmails}`,
     default: `${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`
   },
-  [events.SUBMIT_SUBCRIPTION_FAILED]: {
+  [events.SUBMIT_SUBSCRIPTION_FAILED]: {
     [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.BPS]: `${alertConfig.bpsEmails};${alertConfig.devTeamEmails}`,
     default: `${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`
   },
-  [events.RETURN_SUBCRIPTION_FAILED]: {
+  [events.RETURN_SUBSCRIPTION_FAILED]: {
     [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
     [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
