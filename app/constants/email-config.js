9253b2266f6e8a93d51cfce3e5c1a66727d7d9aa
John Barnard <john.barnard.external@eviden.com>
2025-02-13 13:21:01 +0000

    Only send emails to dev emails

--- app/constants/email-config.js
+++ app/constants/email-config.js
@@ -1,27 +1,14 @@
 const alertConfig = require('../config/alert')
 const events = require('./events')
-const sourceSystems = require('./source-systems')
 
 module.exports = {
   [events.PROCESSING_SUBSCRIPTION_FAILED]: {
-    [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.BPS]: `${alertConfig.bpsEmails};${alertConfig.devTeamEmails}`,
-    default: `${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`
+    default: `${alertConfig.devTeamEmails}`
   },
   [events.SUBMIT_SUBSCRIPTION_FAILED]: {
-    [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.BPS]: `${alertConfig.bpsEmails};${alertConfig.devTeamEmails}`,
-    default: `${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`
+    default: `${alertConfig.devTeamEmails}`
   },
   [events.RETURN_SUBSCRIPTION_FAILED]: {
-    [sourceSystems.DELINKED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.SFI_EXPANDED]: `${alertConfig.opsAnalysisEmails};${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.CS]: `${alertConfig.csEmails};${alertConfig.devTeamEmails}`,
-    [sourceSystems.BPS]: `${alertConfig.bpsEmails};${alertConfig.devTeamEmails}`,
-    default: `${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`
+    default: `${alertConfig.devTeamEmails}`
   }
 }
