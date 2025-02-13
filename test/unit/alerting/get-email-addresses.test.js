058062129b2b9a278a1b40e5df8b4bb5e5c5714f
John Barnard <john.barnard.external@eviden.com>
2025-02-13 13:26:16 +0000

    Only send emails to dev emails

--- test/unit/alerting/get-email-addresses.test.js
+++ test/unit/alerting/get-email-addresses.test.js
@@ -12,21 +12,21 @@ const { getEmailAddresses } = require('../../../app/alerting/get-email-addresses
 describe('get email addresses', () => {
   test('should return correct email addresses for PROCESSING_SUBSCRIPTION_FAILED', () => {
     const emails = getEmailAddresses(PROCESSING_SUBSCRIPTION_FAILED)
-    expect(emails).toEqual(`${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`)
+    expect(emails).toEqual(`${alertConfig.devTeamEmails}`)
   })
 
   test('should return correct email addresses for SUBMIT_SUBSCRIPTION_FAILED', () => {
     const emails = getEmailAddresses(SUBMIT_SUBSCRIPTION_FAILED)
-    expect(emails).toEqual(`${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`)
+    expect(emails).toEqual(`${alertConfig.devTeamEmails}`)
   })
 
   test('should return correct email addresses for RETURN_SUBSCRIPTION_FAILED', () => {
     const emails = getEmailAddresses(RETURN_SUBSCRIPTION_FAILED)
-    expect(emails).toEqual(`${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`)
+    expect(emails).toEqual(`${alertConfig.devTeamEmails}`)
   })
 
   test('should return correct email addresses for ETL_PROCESS_ERROR', () => {
     const emails = getEmailAddresses(ETL_PROCESS_ERROR, 'default')
-    expect(emails).toEqual(`${alertConfig.demographicsEmails};${alertConfig.devTeamEmails}`)
+    expect(emails).toEqual(`${alertConfig.devTeamEmails}`)
   })
 })
