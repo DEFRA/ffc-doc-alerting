60282fc6dd5df2a5f37129dbca7b85decfd7590e
Sam Plackett <samuel.plackett@eviden.com>
2025-06-19 15:08:05 +0100

    fixed email config

--- app/constants/email-config.js
+++ app/constants/email-config.js
@@ -13,5 +13,8 @@ module.exports = {
   },
   [events.RETURN_SUBSCRIPTION_FAILED]: {
     default: `${alertConfig.devTeamEmails}`
+  },
+  [events.ETL_PROCESS_ERROR]: {
+    default: `${alertConfig.devTeamEmails}`
   }
 }
