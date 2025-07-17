98310804423f7acc0c4d1fa74451df4bfb4b172b
John Barnard <john.barnard.external@eviden.com>
2025-07-17 14:43:39 +0100

    Added publish error
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/constants/events.js
+++ app/constants/events.js
@@ -2,5 +2,6 @@ module.exports = {
   PROCESSING_SUBSCRIPTION_FAILED: 'uk.gov.defra.ffc.doc.warning.processing.subscription.failed',
   SUBMIT_SUBSCRIPTION_FAILED: 'uk.gov.defra.ffc.doc.warning.submit.subscription.failed',
   RETURN_SUBSCRIPTION_FAILED: 'uk.gov.defra.ffc.doc.warning.return.subscription.failed',
-  ETL_PROCESS_ERROR: 'uk.gov.defra.ffc.doc.warning.etl.process.error'
+  ETL_PROCESS_ERROR: 'uk.gov.defra.ffc.doc.warning.etl.process.error',
+  PUBLISH_ERROR: 'uk.gov.defra.ffc.doc.warning.publish.error',
 }
