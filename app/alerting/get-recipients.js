a73bb65bdacf638ff9faf13d8fb88460a2e7fdd8
Sam Plackett <samuel.plackett@eviden.com>
2025-10-15 14:39:00 +0100

    use replace all

--- app/alerting/get-recipients.js
+++ app/alerting/get-recipients.js
@@ -2,7 +2,7 @@ const { getEmailAddresses } = require('./get-email-addresses')
 
 const getRecipients = (event) => {
   const emailAddresses = getEmailAddresses(event.type)
-  return emailAddresses ? emailAddresses.replace(/\s/g, '').trim().split(';').filter(x => x.length) : []
+  return emailAddresses ? emailAddresses.replaceAll(/\s/g, '').trim().split(';').filter(x => x.length) : []
 }
 
 module.exports = {
