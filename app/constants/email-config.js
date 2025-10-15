a9858cd47e4c947c2198d480ff860b437ced2a7e
Sam Plackett <samuel.plackett@eviden.com>
2025-10-15 11:12:40 +0100

    add new alert type

--- app/constants/email-config.js
+++ app/constants/email-config.js
@@ -2,28 +2,5 @@ const alertConfig = require('../config/alert')
 const events = require('./events')
 
 module.exports = {
-  [events.PROCESSING_SUBSCRIPTION_FAILED]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.SUBMIT_SUBSCRIPTION_FAILED]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.RETURN_SUBSCRIPTION_FAILED]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.ETL_PROCESS_ERROR]: {
-    default: `${alertConfig.devTeamEmails};${alertConfig.dwhEmails}`
-  },
-  [events.PUBLISH_ERROR]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.ZERO_VALUE_STATEMENT]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.DATA_PROCESSING_ERROR]: {
-    default: `${alertConfig.devTeamEmails}`
-  },
-  [events.DATA_PUBLISHING_ERROR]: {
-    default: `${alertConfig.devTeamEmails}`
-  }
+  [events.ETL_PROCESS_ERROR]: `${alertConfig.dwhEmails}`
 }
